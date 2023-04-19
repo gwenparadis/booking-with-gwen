e9cc28f8e375dffcb9efde1782ff60338128ea58
gwenparadis <gwendelynparadis2@gmail.com>
2023-04-19 14:50:04 -0400

    debug and add header, layout, login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
 import IndexPage from "./pages/IndexPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Route>
     </Routes>
   );
 }
