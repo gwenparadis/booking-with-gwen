9a198df28c5395d83513063719364629fcace5e6
gwenparadis <gwendelynparadis2@gmail.com>
2023-04-22 17:08:58 -0400

    register page and express set up

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function registerUser(ev) {
+    ev.preventDefault();
+    axios.get("http://localhost:4000/test");
+  }
+
+  return (
+    <div className="mt-4 grow flex item-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <form className="max-w-md mx-auto">
+          <input type="name" placeholder="name"></input>
+          <input type="email" placeholder="your@email.com"></input>
+          <input type="password" placeholder="password"></input>
+          <button className="primary" type="register">
+            Register
+          </button>
+          <div className="text-center py-2 text-gray-500">
+            Already a Member? {""}
+            <Link className="underline text-black" to={"/login"}>
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
