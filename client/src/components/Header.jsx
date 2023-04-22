da46f5f18906eebacf40fbc5e0853b141276f833
gwenparadis <gwendelynparadis2@gmail.com>
2023-04-22 16:38:31 -0400

    login page creation and styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="p-4 flex justify-between">
+    <header className="flex justify-between">
       <a href="" className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +58,7 @@ export default function Header() {
             />
           </svg>
         </Link>
-        <button>
+        <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -73,7 +73,7 @@ export default function Header() {
               d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
             />
           </svg>
-        </button>
+        </div>
       </div>
     </header>
   );
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,28 @@
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-  return "login form should go here";
+  return (
+    <div className="mt-4 grow flex item-center justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4"> Login</h1>
+        <form className="max-w-md mx-auto">
+          <input
+            className="border rounded-full"
+            type="email"
+            placeholder="your@email.com"
+          ></input>
+          <input type="password" placeholder="password"></input>
+          <button className="primary" type="login">
+            Login
+          </button>
+          <div className="text-center py-2 text-gray-500">
+            Dont have an account yet?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Register now
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
