a1ba07a13532981dd52240019b350f4a6c93aea9
gwenparadis <gwendelynparadis2@gmail.com>
2023-05-30 11:37:04 -0400

    added jsonwebtoken successfully when log in is complete

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 require("dotenv").config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "r4nd0m!dA!Vcin6 !#@$ yaaaaa";
 
 app.use(express.json());
 app.use(
@@ -40,7 +42,15 @@ app.post("/login", async (req, res) => {
   if (userDoc) {
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-      res.json("password ok");
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json("password ok");
+        }
+      );
     } else {
       res.status(422).json("password not ok");
     }
