0d4a921f2b0c4ecd699345d895a6c52bfbd7ddea
gwenparadis <gwendelynparadis2@gmail.com>
2023-06-06 11:06:51 -0400

    header routes, Account page and Appjsx route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout.jsx";
 import IndexPage from "./pages/IndexPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import AccountPage from "./pages/AccountPage";
 import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 
@@ -18,6 +19,7 @@ function App() {
           <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<AccountPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
 
   return (
     <header className="flex justify-between">
-      <a href="" className="flex items-center gap-1">
+      <Link to={"/"} className="flex items-center gap-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -23,7 +23,7 @@ export default function Header() {
           />
         </svg>
         <span className="font-bold text-xl">airbnb</span>
-      </a>
+      </Link>
       <div className="flex gap-2 border border-color-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
         <div className="border-l border-gray-300">Any week</div>
@@ -45,23 +45,24 @@ export default function Header() {
           </svg>
         </button>
       </div>
-      <div className="flex gap-2 border border-color-gray-300 rounded-full py-2 px-4">
-        <Link to={"/login"}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-        </Link>
+      <Link
+        to={user ? "/account" : "/login"}
+        className="flex gap-2 border border-color-gray-300 rounded-full py-2 px-4"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
         <div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +80,7 @@ export default function Header() {
           </svg>
         </div>
         {!!user && <div>{user.name}</div>}
-      </div>
+      </Link>
     </header>
   );
 }
