f267f34ce7b776ca230050d5e98a5263b9658d8f
gwenparadis <gwendelynparadis2@gmail.com>
2023-06-06 11:46:51 -0400

    styling and nav bar account page

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,3 +1,44 @@
+import { useContext } from "react";
+import { UserContext } from "../UserContext";
+import { Link, Navigate, useParams } from "react-router-dom";
+
 export default function AccountPage() {
-  return <div>Account Page!!</div>;
+  const { ready, user } = useContext(UserContext);
+
+  if (!ready) {
+    return "Loading...";
+  }
+
+  if (ready && !user) {
+    return <Navigate to={"/login"} />;
+  }
+
+  const { subpage } = useParams();
+
+  function linkClasses(type) {
+    let classes = "py-2 px-6";
+    if (type === subpage || (subpage === undefined && type === "profile")) {
+      classes += " bg-primary text-white rounded-full";
+    }
+    return classes;
+  }
+
+  return (
+    <div>
+      <nav className="w-full flex justify-center mt-8 gap-2">
+        <Link to={"/account"} className={linkClasses("profile")}>
+          My Profile
+        </Link>
+        <Link to={"/account/bookings"} className={linkClasses("bookings")}>
+          My Bookings
+        </Link>
+        <Link
+          to={"/account/accomodations"}
+          className={linkClasses("accomodations")}
+        >
+          My Accomodations
+        </Link>
+      </nav>
+    </div>
+  );
 }
