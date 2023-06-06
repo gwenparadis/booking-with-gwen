d39c87f553dc83f6ded7e6e803a5816b195ca1c8
gwenparadis <gwendelynparadis2@gmail.com>
2023-06-06 12:14:05 -0400

    logout function and redirect

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,19 +1,29 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../UserContext";
 import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage() {
-  const { ready, user } = useContext(UserContext);
+  const [redirect, setRedirect] = useState(null);
+  const { ready, user, setUser } = useContext(UserContext);
+  let { subpage } = useParams();
+  if (subpage === undefined) {
+    subpage = "profile";
+  }
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
+    setUser(null);
+  }
 
   if (!ready) {
     return "Loading...";
   }
 
-  if (ready && !user) {
+  if (ready && !user && !redirect) {
     return <Navigate to={"/login"} />;
   }
-
-  const { subpage } = useParams();
 
   function linkClasses(type) {
     let classes = "py-2 px-6";
@@ -23,9 +33,13 @@ export default function AccountPage() {
     return classes;
   }
 
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
   return (
     <div>
-      <nav className="w-full flex justify-center mt-8 gap-2">
+      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
         <Link to={"/account"} className={linkClasses("profile")}>
           My Profile
         </Link>
@@ -39,6 +53,14 @@ export default function AccountPage() {
           My Accomodations
         </Link>
       </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-w-lg mx-auto">
+          Logged in as {user.name} ({user.email})<br />
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
